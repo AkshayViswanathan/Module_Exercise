140841265711777c7c54c40067763651da93a2c3
AkshayViswanathan <akshayviswanathan@techversantinfotech.com>
2024-01-03 10:57:22 +0530

    updated exe 8

--- exe8.js
+++ exe8.js
@@ -23,6 +23,16 @@ depositeAmount(despoite){
     console.log(`You deposited: ${despoite} , Total amount : ${this.accountBalance} `)
 }
 
+withdrawAmount(Withdraw){
+    if(Withdraw <= this.accountBalance){
+        this.accountBalance = this.accountBalance - Withdraw
+        console.log(`Sucessfully withdrawn: ${Withdraw}, your current balance:${this.accountBalance}`);
+    } else{
+        console.log("InSufficient fund");
+    }
+
+}
+
 }
 
 Bank.prototype.fullname = function(){
@@ -36,8 +46,9 @@ let person3 = new Bank("ajay", "kumar", "123562345", 5000)
 let person4 = new Bank("pranav", "babu", "123562345", 2000)
 
 
+person2.fullname()  
 person1.accountdeatils()
-person2.fullname()   
 person3.checkBalance()
 person4.depositeAmount(500)
-person3.depositeAmount(500)
+person3.depositeAmount(1000)
+person1.withdrawAmount(100)

b749814795190f82e252935de5e61ce327eb8c10
AkshayViswanathan <akshayviswanathan@techversantinfotech.com>
2024-01-22 11:45:13 +0530

    edit button error

--- src/components/DialogBox.vue
+++ src/components/DialogBox.vue
@@ -4,54 +4,63 @@
     v-model="dialogVisible"
     title="Tips"
     width="30%"
-  class="dialogBoc"
-  >
-  <span>This is a message</span>
-    <template #footer> 
-        <span class="dialog-footer">
+  class="dialogBoc">
+  <div class="input1">
+    
+    <label for="email" class="lable1">Enter email</label>
+    <el-input type="text" 
+                
+                  placeholder="@gmail.com" />
+                 
 
+</div> 
 
-     <div class="input2">
-            <label class="lable2"> password</label>
-   
-            <el-input           
-                 type="password"
-                   placeholder="password"
-                     show-password/>
-                   </div>
-   
+<div class="input2">
+      <label class="lable2"> password</label>
 
-   <div class="input2">
-            <label class="lable2"> Confirm password</label>
-   
-            <el-input
-              
-                 type="password"
-                   placeholder="confirm password"
-                     show-password/>
-                   
- 
-   </div>
- 
+      <el-input
   
- <div id="divbox">
-   <div style="margin-top: 20px" class="radiobutton">
-   <el-radio-group >
-     <el-radio-button label="Female" >Female</el-radio-button>
-     <el-radio-button label="Male" > Male</el-radio-button>
-   </el-radio-group>
-  
- </div>
+           type="password"
+             placeholder="password"
+               show-password/>
+</div>
 
- </div>
+<div class="input2">
+      <label class="lable2"> Confirm password</label>
 
-        </span>
-    </template>
-     
+      <el-input
+           type="password"
+             placeholder="confirm password"
+               show-password/>
+           
 
+</div>
+
+
+<div id="divbox">
+<div style="margin-top: 20px" class="radiobutton">
+<el-radio-group >
+<el-radio-button label="Female" >Female</el-radio-button>
+<el-radio-button label="Male" > Male</el-radio-button>
+</el-radio-group>
+
+</div>
+</div>
+
+<div class="dropdown">  
+
+<el-select clearable placeholder="Select">
+<el-option
+v-for="item in options"
+:key="item.value"
+:label="item.label"
+:value="item.value"
+/> 
+</el-select>
 
 
 </el-dialog>
+
 
 </template>
 
